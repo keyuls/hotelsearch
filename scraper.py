e2c546930a0b25d5514bd508d22d71e73225c53a
keyulpax <keyul.shah@pax.us>
2017-06-29 15:39:50 -0700

    sorting data

--- scraper.py
+++ scraper.py
@@ -20,10 +20,21 @@ def searchSite():
     pm.close()
     pm.join()
     #merged=heapq.merge(results)
-    for x in results:
+    #yo= min(data[0]['ecstasy'] for data in results)
+    #print(yo)
+    while len(results)>0:
+           minObj = [data[-1]['ecstasy'] for data in results]
+           print(minObj)
+           minIndex = minObj.index(min(minObj))
+           store.append(results[minIndex][-1])
+           results[minIndex].pop()
+           if len(results[minIndex])==0:
+                  results.pop(minIndex)
+    store.reverse()       
+    '''for x in results:
         for y in x:
             store.append(y)
-    store.sort(key=sortByEcstasy,reverse=True)
+    store.sort(key=sortByEcstasy,reverse=True)'''
     return store
 
 def scrapSite(site):
