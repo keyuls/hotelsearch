f0593723a0f84bd23536ed57d53b3b48dd19acf6
keyuls <keyul@asu.edu>
2017-06-29 23:29:17 -0700

    working code and format updation

--- scraper.py
+++ scraper.py
@@ -2,48 +2,31 @@ from flask import make_response
 import requests
 import json
 from multiprocessing.dummy import Pool as ThreadPool
-import heapq
 
+collect=[]
 
 def performAction(action):
-    #if action=="search":
-       result = searchSite()
-       result ={"results":result}
+       result ={"results":searchSite()}
        response = jsonResponse(result)
        return response
 
 def searchSite():
-    sites=['Expedia','Orbitz','Priceline','Travelocity','Hilton']
+    sites =['Expedia','Orbitz','Travelocity','Priceline','Hilton']
     store=[]
-    pm = ThreadPool(5)
-    results = pm.map(scrapSite,sites)
+    pm = ThreadPool(processes=5)
+    results = pm.map_async(scrapSite,sites)
     pm.close()
-    pm.join()
-    #merged=heapq.merge(results)
-    #yo= min(data[0]['ecstasy'] for data in results)
-    #print(yo)
-    while len(results)>0:
-           minObj = [data[-1]['ecstasy'] for data in results]
-           print(minObj)
-           minIndex = minObj.index(min(minObj))
-           store.append(results[minIndex][-1])
-           results[minIndex].pop()
-           if len(results[minIndex])==0:
-                  results.pop(minIndex)
-    store.reverse()       
-    '''for x in results:
-        for y in x:
-            store.append(y)
-    store.sort(key=sortByEcstasy,reverse=True)'''
-    return store
+    pm.join()       
+    return collect
 
 def scrapSite(site):
     baseurl="http://localhost:9000/scrapers/"
-    url = baseurl+site
+    url = baseurl+site 
     result = requests.get(url)
     result = result.json()
-    result = result.get("results")
-    return result
+    collect.extend(result.get("results"))
+    collect.sort(key=sortByEcstasy,reverse=True)
+    return
 
 def jsonResponse(result):
     res = json.dumps(result, indent=4)
